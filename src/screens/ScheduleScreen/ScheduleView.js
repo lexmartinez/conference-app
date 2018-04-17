3465effcc3fdef8f38a7aae62b6a4d5f6171d671
Lex Martinez <lex0316@gmail.com>
2018-04-17 14:19:46 -0500

    ScheduleView - Details Modal

--- src/screens/ScheduleScreen/ScheduleView.js
+++ src/screens/ScheduleScreen/ScheduleView.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, SectionList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { ListItem, SearchBar } from 'react-native-elements';
+import Modal from 'react-native-modal';
 import config from '../../config';
+import { Button, Card } from 'react-native-elements';
 
 class ScheduleView extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      conference: {events: []}
+      conference: {events: []},
+      event: {},
+      modal: false,
+      events: []
     };
   }
 
   componentDidMount() {
     const { getInfo } = this.props;
     getInfo();
+    this.setState({event: {},modal: false,
+      events: []})
   }
 
   componentWillReceiveProps(nextProps) {
@@ -24,25 +31,69 @@ class ScheduleView extends Component {
         conference: nextProps.conference,
         error: nextProps.error,
         completed: nextProps.completed,
-        events: nextProps.events
+        modal: false,
+        events: (nextProps.conference||[]).events
       });
     }
   }
 
   render() {
-    console.log(this.state)
     return (
       <View>
+        <SearchBar
+          icon={{ type: 'feather', name: 'search' }}
+          clearIcon={{ type: 'feather', name: 'x' }}
+          onChangeText={(search) => {
+            if(search === ''){
+              this.setState({events:this.props.conference.events});
+            } else {
+              this.setState({events:this.props.conference.events.filter(item => {
+                console.log(item)
+                item.name.toLowerCase().indexOf(search.toLowerCase())>-1 ||
+                item.description.toLowerCase().indexOf(search.toLowerCase())>-1
+              })});
+            }
+          }}
+          placeholder='Search ...' 
+          cancelButtonTitle="Cancel"/>
         <SectionList
-          renderItem={({ item, index, section }) => <ListItem containerStyle={{borderLeftColor: item.color, borderLeftWidth: 6}} key={index} title={item.name} subtitle={item.time + ' ' + item.place}/>}
+          renderItem={({ item, index, section }) => <ListItem containerStyle={{borderLeftColor: item.color, borderLeftWidth: 6}} key={index} title={item.name} subtitle={item.time + ' ' + item.place} onPress={()=>this.setState({event: item, modal: true})}/>}
           renderSectionHeader={({ section: { title } }) => <ListItem title={title}
                   containerStyle={{backgroundColor: config.SCHEDULE_HEADER_COLOR}}
                   titleStyle={{ color:config.PRIMARY_BG_COLOR, fontWeight: '800'}}
                   chevronColor={config.SCHEDULE_HEADER_COLOR}
                   stickySectionHeadersEnabled={true}
           />}
-          sections={this.props.conference.events || []}
+          sections={this.state.events || []}
           keyExtractor={(item, index) => item + index} />
+
+          {
+            <Modal isVisible={this.state.modal}
+            onSwipe={() => this.setState({ modal: false })}
+            onBackdropPress={() => this.setState({ modal: false })}>
+              <Card title={(this.state.event.name||'').toUpperCase()}>
+                <View>
+                  <Text style={{fontWeight:'700'}}>{this.state.event.time} {this.state.event.place}</Text>
+                  <Text style={{textAlign: 'justify', marginTop: 10}}>{this.state.event.description}</Text>
+                  {
+                    this.state.event.speaker?
+                      <View style={{flexDirection:'row', marginTop: 10}}>
+                        <Text style={{fontWeight:'700'}}>Speaker: </Text>
+                        <Text>{this.state.event.speaker}</Text>
+                      </View>:null
+                  }
+                  <Button
+                    title='CLOSE'
+                    buttonStyle={{
+                      backgroundColor: config.PRIMARY_BG_COLOR,
+                      marginTop: 15
+                    }}
+                    onPress={()=>this.setState({modal: false})}
+                  />
+                </View>
+              </Card>
+            </Modal>
+          }
       </View>
     );
   }
